5f86ebcb521045d053b2f015d668b744f7f7cd7e
Abullah Irfan <abdullahirfanhps@gmail.com>
2024-08-23 17:30:26 +0500

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/navbar"
 
+
 function App() {
 
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,81 @@
+
+import  { useState, useEffect } from 'react';
 import Styles from "./NavBar.module.css"
+// import { useEffect } from "react";
 
 function NavBar() {
+
+    const [scrolled , setScrolled] = useState(false);
+    const [showSidebar , setShowSidebar] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+          const offset = window.scrollY;
+          if (offset > 0) {
+            setScrolled(true);
+          } else {
+            setScrolled(false);
+          }
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+
+      const toggleSidebar = () => {
+        setShowSidebar(!showSidebar);
+    };
+
+
+
+
+
+    // function showSidebar(){
+    //     const sidebar = document.querySelector('.sidebar')
+    //     sidebar.style.display = 'flex'
+    //   }
+    //   function hideSidebar(){
+    //     const sidebar = document.querySelector('.sidebar')
+    //     sidebar.style.display = 'none'
+    //   }
+
+
     return (
         <div>
-            <nav className={Styles.MainNav} >
-                <div className={Styles.Logo}>
-                    <h2><strong>Better Off</strong></h2>
-                </div>
+            <nav>
+                <ul className={`${Styles.sidebar} ${showSidebar ? Styles.show : ''}`}>
+                  <li  onClick={toggleSidebar} ><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 96 960 960" width="26"><path d="m249 849-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"/></svg></a></li>
+                  <hr className={Styles.sidebarhr}></hr>
+                  <li><a href="#">Overview</a></li>
+                  <hr className={Styles.sidebarhr}></hr>
+                  <li><a href="#">Work</a></li>
+                  <hr className={Styles.sidebarhr}></hr>
+                  <li><a href="#">Feed</a></li>
+                  <hr className={Styles.sidebarhr}></hr>
+                </ul>
+                
+                <ul className={`${Styles.navbar} ${scrolled ? Styles.scrolled : ''}`} >
+                  <li><a href="#"><strong>Better Offf</strong></a></li>
+                  <div className={Styles.menulink} >
+                  <li className={Styles.hideOnMobile}><a href="#">Overview</a></li>
+                  <li className={Styles.hideOnMobile}><a href="#">Work</a></li>
+                  <li className={Styles.hideOnMobile}><a href="#">Feed</a></li>
+                  </div>
+                  <li className={Styles.menubutton}  onClick={toggleSidebar}  ><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 96 960 960" width="26"><path d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/></svg></a></li>
+                  <li className={Styles.hideOnMobile}><a href="#">Pricing</a></li>
+                </ul>
+            
             </nav>
+
+            <h1>BETTER OFF</h1>
+            <h2>BETTER OFF</h2>
+
         </div>
     )
 }
 
 export default NavBar
+
