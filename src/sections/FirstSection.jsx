91091b619eda8c7fb25ec8c3a5a151a153fba2ab
Abullah Irfan <abdullahirfanhps@gmail.com>
2024-09-03 20:40:03 +0500

    corrected animations

--- src/sections/FirstSection.jsx
+++ src/sections/FirstSection.jsx
@@ -5,26 +5,100 @@ import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
     
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Draggable } from "gsap/Draggable";
 import { useRef } from "react";
 
 
-gsap.registerPlugin(useGSAP,ScrollTrigger);
+gsap.registerPlugin(useGSAP,ScrollTrigger,Draggable);
 
 function FirstSection() {
 
-    const subscribe = useRef()
+    const move1 = useRef()
+    const move2 = useRef()
+    const move3 = useRef()
+    const move4 = useRef()
+    const move5 = useRef()
 
+    const subscribe = useRef()
+    const Subscribe = useRef()
+
+            
+        
 
     useGSAP(() => {
-        gsap.to(subscribe.current, {
-            x: '-170%', 
-            duration: 25, 
-            repeat: -1,  
-            ease: 'linear', 
-      
-          
+        gsap.to( move1.current, {
+            transform: "translateX(-100%)",
+            repeat: -1,
+            duration: 8,
+            ease: "none"
+            
+            
+        });
+    });
+    useGSAP(() => {
+        gsap.to( move2.current, {
+            transform: "translateX(-100%)",
+            repeat: -1,
+            duration: 8,
+            ease: "none"
+            
+            
+        });
+        
+    });
+
+    useGSAP(() => {
+        gsap.to( move3.current, {
+            transform: "translateX(-100%)",
+            repeat: -1,
+            duration: 8,
+            ease: "none"
+            // ease: "inOut",
+            
+        });
+    });
+    useGSAP(() => {
+        gsap.to( move4.current, {
+            transform: "translateX(-100%)",
+            repeat: -1,
+            duration: 8,
+            ease: "none"
+            // ease: "inOut",
+            
+        });
+    });
+    useGSAP(() => {
+        gsap.to( move5.current, {
+            transform: "translateX(-100%)",
+            repeat: -1,
+            duration: 8,
+            ease: "none"
+            // ease: "inOut",
+            
         });
       });
+    
+      
+
+      useGSAP(() => {
+        gsap.to(Subscribe.current, {
+            scale : 1.8,
+            scrollTrigger: {
+                trigger: Subscribe.current,
+                scroller: "body",
+                start: "top 450rem",
+                end: "450rem",
+                scrub: true,
+              }
+          
+        });
+        Draggable.create(Subscribe.current, {
+            type: "x",
+            // bounds: Subscribe.current,   
+            inertia: true, 
+        });
+      });
+
 
     return (
         <>
@@ -38,10 +112,32 @@ function FirstSection() {
                     for growing businesses. Packaged as <br />
                     subscriptions or bundled projects.</h1>
             </div>
-            <div className={Styles.subscribe} ref={subscribe} >
-                <h1>SUBSCRIBESUBSCRIBESUBSCRIBESUBSCRIBESUBSCRIBE </h1>
-                <img src="https://www.datocms-assets.com/106915/1717687183-betteroffstudio_work-loop_11.jpg?auto=format%2Ccompress&fit=max&h=800&w=800" alt="" />
+            <div className={Styles.subscribe} ref={Subscribe}  >
+                <div className={Styles.move} ref={move1} >
+                    <h1  ref={subscribe}>SUBSCRIBE </h1>
+                    <img src="https://www.datocms-assets.com/106915/1717687183-betteroffstudio_work-loop_11.jpg?auto=format%2Ccompress&fit=max&h=800&w=800" alt="" />
+                </div>
+                <div className={Styles.move} ref={move2} >
+                    <h1 ref={subscribe}>SUBSCRIBE </h1>
+                    <img src="https://www.datocms-assets.com/106915/1717687183-betteroffstudio_work-loop_11.jpg?auto=format%2Ccompress&fit=max&h=800&w=800" alt="" />
+                </div>
+                <div className={Styles.move} ref={move3} >
+                    <h1 ref={subscribe}>SUBSCRIBE </h1>
+                    <img src="https://www.datocms-assets.com/106915/1717687183-betteroffstudio_work-loop_11.jpg?auto=format%2Ccompress&fit=max&h=800&w=800" alt="" />
+                </div>
+                <div className={Styles.move} ref={move4} >
+                    <h1 ref={subscribe}>SUBSCRIBE </h1>
+                    <img src="https://www.datocms-assets.com/106915/1717687183-betteroffstudio_work-loop_11.jpg?auto=format%2Ccompress&fit=max&h=800&w=800" alt="" />
+                </div>
+                <div className={Styles.move} ref={move5} >
+                    <h1 ref={subscribe}>SUBSCRIBE </h1>
+                    <img src="https://www.datocms-assets.com/106915/1717687183-betteroffstudio_work-loop_11.jpg?auto=format%2Ccompress&fit=max&h=800&w=800" alt="" />
+                </div>
             </div>
+                
+
+            
+            <h1 className={Styles.vhbjs}>hvbajks</h1>
         </>
     )
 }
